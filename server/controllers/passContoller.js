751f969f1b5d719f8dda8e480d9d81bf64d786eb
Festus <festusgitahik@gmail.com>
2023-08-29 05:17:40 +0300

    setup nodemailer for forgor password

--- server/controllers/passContoller.js
+++ server/controllers/passContoller.js
@@ -1,5 +1,6 @@
 const connection = require("../db");
 const bcrypt = require("bcrypt");
+const nodemailer = require("nodemailer");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 dotenv.config();
@@ -102,7 +103,64 @@ const handleForgotPass = (req, res) => {
         console.log(link);
 
         // send the link via email
-        return res.json("doen");
+        const transporter = nodemailer.createTransport({
+          service: "gmail",
+          auth: {
+            user: "wschool752@gmail.com",
+            pass: process.env.MAIL,
+          },
+        });
+
+        // html contenet
+        const htmlContent = `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Password Reset Request</title>
+        </head>
+        <body style="font-family: Arial, sans-serif; margin: 0; padding: 0; background-color: #f4f4f4;">
+            <table style="width: 100%; max-width: 600px; margin: 0 auto; background-color: #ffffff; padding: 20px;">
+                <tr>
+                    <td style="text-align: center; background-color: #007bff; padding: 10px;">
+                        <h1 style="color: #ffffff;">Password Reset Request</h1>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="padding: 20px;">
+                        <p>Hello,</p>
+                        <p>We have received a request to reset your password. If you did not request this change, please disregard this email.</p>
+                        <p>To reset your password, please click the following link:</p>
+                        <p><a href=${link}>Password Reset Link</a></p>
+                        <p>This link is valid for the next 5 minutes. If you have any questions or concerns, please don't hesitate to contact our support team.</p>
+                        <p>Best regards,<br>Cyber Safe</p>
+                    </td>
+                </tr>
+            </table>
+        </body>
+        </html>
+        `;
+
+        const mailOptions = {
+          from: "wschool752@gmail.com",
+          to: user.email,
+          subject: "Password Reset Request",
+          html: htmlContent,
+        };
+
+        mailOptions.headers = {
+          "Content-Type": "text/html",
+        };
+
+        // send email
+        transporter.sendMail(mailOptions, function (error, info) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log("Email sent: " + info.response);
+          }
+        });
+        return res.json({ message: "email sent" });
       }
     );
   } catch (error) {
